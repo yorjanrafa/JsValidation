5a4779ed959081314f0f3340ac82b07c0bcfbe5a
yorjanrafa <66923631+yorjanrafa@users.noreply.github.com>
2022-08-06 18:56:54 -0400

    Update VAlidations js

--- validations.js
+++ validations.js
@@ -1,55 +1,68 @@
+// antes de usar el script asegurese de haber agragado a su
+// formulario en el  HTML el id de form ejemplo: <form id="form">
+// seguido de la etiqueta novalidate para evitar la validacion
+// por defecto del navegador ejemplo: <form id="form" novalidate>
+
 // agregue el patron aca que desea validar o verificar
 const regex = {
   numeric: /^\d{1,3}$/,
-  email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  email:   /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 };
 
 // se agregan los estilos para alertar del error con su respectivo mensaje
 const addInvalidStyle = (input, message) => {
   input.classList.add("is-invalid");
-  let parentInput = input.parentNode;
 
-  if (parentInput.childNodes[3] == undefined) {
-    let paragraph = document.createElement("p");
-    let strong = document.createElement("strong");
-    strong.appendChild(document.createTextNode(message));
-    paragraph.appendChild(strong);
-    paragraph.classList.add("invalid-feedback");
-    paragraph.id = "message";
+  // crea el nodo de parrafo a mostrar "<p></p>"
+  let parentInput  = input.parentNode;
+  let paragraph    = document.createElement("p");
+  let strong       = document.createElement("strong");
+  strong   .appendChild(document.createTextNode(message));
+  paragraph.appendChild(strong);
+  paragraph.classList.add("invalid-feedback");
+
+  // vera con regualaridad el """parentInput.childNodes[3]""" el cual es la
+  // posicion en la cual se encuentra situado el paragraph en el html si por
+  // algun motivo usted lo tiene de 4 o 5 sustituir el 3 por el numero de nodo
+  let childNode = parentInput.childNodes[3];
+  if (!childNode) {
     parentInput.appendChild(paragraph);
+  } else {
+    if (/error/g.test(childNode.id)) {
+      childNode.remove();
+      parentInput.appendChild(paragraph);
+    }
   }
 };
 
-
-// se eliminan los estilos del error en caso 
+// se eliminan los estilos del error en caso
 // de que la verificacion sea exitosa
 const removeInvalidStyle = (input) => {
   input.classList.remove("is-invalid");
 };
 
-
-// especificar el aca el tipo de mensajes
-// que desea mostrar segun el tipo
+// especificar aca el tipo de mensajes
+// que desea mostrar segun el tipo de dato
 const messages = {
-  numeric: "Solo caracteres numericos",
-  email: "Solo direcciones de coreo electronico",
-  alphabetic: "Solo caracteres alfabeticos",
+  numericIDPC: "Sólo valores numericos de 1 a 3 caracteres",
+  email:       "Solo direcciones de coreo electronico",
+  alphabetic:  "Solo caracteres alfabeticos",
 };
 
-
-// se se crea una comprobacion en base a un ternario
-// en caso de que exista el error se agregaran los estilos
-// y se enviara el mensaje a mostrar
+// se crea una comprobacion en base a un ternario
+// en caso de que exista el error se agregaran los
+// estilos y se enviara el mensaje a mostrar
+// NOTA: esto grita "HASME TUYA, PERO PRIMERO REFACTORIZAME DOY ASCO"
 const validations = {
   numericField: function () {
     regex["numeric"].test(this.value)
       ? removeInvalidStyle(this)
-      : addInvalidStyle(this, messages["numeric"]);
+      : addInvalidStyle   (this, messages["numericIDPC"]);
   },
   emailField: function () {
     regex["email"].test(this.value)
       ? removeInvalidStyle(this)
-      : addInvalidStyle(this, messages["email"]);
+      : addInvalidStyle   (this, messages["email"]);
   },
 };
 
@@ -65,26 +78,18 @@ const validations = {
    EJEMPLO:    id_nombre : validations["alphabeticField"]
 
    NOTA FINAL: los id deben ser los mismos a los ids de cada uno de los 
-   input del formulario y el tipo de validacion a asignar debe ser 
-   correspondiente al tipo de dato que se espera
+   inputs correspondiente al tipo de dato que se espera
 */
 const ids = {
   id_number_id: validations["numericField"],
-  id_mark: validations["emailField"],
+  id_mark:      validations["emailField"],
 };
 
-// selecciona el formulario
-const form = document.querySelector("#form");
+// selecciona el formulario siempre y cuando el id de este sea ""form""
+const form   = document.querySelector("#form");
 
 // selecciona cada uno de los inputs del formulario con el id form
 const inputs = document.querySelectorAll("#form input");
-// for (let index = 1; index < formx.length; index++) {
-//   if (ids[formx[index].id]) {
-//     let id = formx[index].id
-//     formx[index].addEventListener("keyup", ids[id]);
-//     formx[index].addEventListener("blur", ids[id]);
-//   }
-// }
 
 /*
    Asignacion de eventos a cada uno de los inputs del formulario
@@ -94,6 +99,5 @@ const inputs = document.querySelectorAll("#form input");
 inputs.forEach((input) => {
   let id = input.id;
   input.addEventListener("keyup", ids[id]);
-  input.addEventListener("blur", ids[id]);
+  input.addEventListener("blur",  ids[id]);
 });
-
